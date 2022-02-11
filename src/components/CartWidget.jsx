d69da2adec015df2d9d2a205be77ba24d218d60d
lquispe <liliana.quispe@gmail.com>
2022-02-11 08:50:43 -0300

    fix style

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faShoppingCart, faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContex';
 
-const CartWidget=()=>(<FontAwesomeIcon icon={faShoppingCart} />)
+const CartWidget = () => {
+
+    const { items } = useContext(CartContext);
+
+    return (
+        <>
+            <div style={{ position: "relative", marginLeft: 6 }}>
+
+                <FontAwesomeIcon icon={faShoppingCart} />
+                <div style={{color: "#fff", 
+                marginLeft: 6, 
+                fontWeight: "bold", 
+                position: "absolute", 
+                left: 6, 
+                top: -2, 
+                fontSize: "0.6rem", 
+                backgroundColor: "black",
+                paddingTop: "2px", 
+                paddingBottom: "2px", 
+                paddingLeft: "5px", 
+                paddingRight: "4px", 
+                borderRadius: "50%"}}>{items.length}</div>
+            </div>
+                
+        </>
+    )
+}
 
 export default CartWidget
